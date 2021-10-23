7cfd99fb29fdc6acc9b0c115616caa5241fb19f0
mbarga akoa anselme germain <germyakoa@gmail.Com>
2021-10-23 09:01:19 +0100

    Add files via upload

---
+++ precache-manifest.29880bde3cefd50fed8db6bc289f9e1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0212fc563ce3564f39d8e51c01a81c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d75bd9554d6a5c8101c",
+    "url": "/static/css/2.039bb34a.chunk.css"
+  },
+  {
+    "revision": "5e60129d95d3120c7901",
+    "url": "/static/css/main.e0efaf70.chunk.css"
+  },
+  {
+    "revision": "3d75bd9554d6a5c8101c",
+    "url": "/static/js/2.b01de000.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/static/js/2.b01de000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e60129d95d3120c7901",
+    "url": "/static/js/main.c26afc48.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "9e45166bafd9358f1e728ed58563e0fd",
+    "url": "/static/media/croise.9e45166b.jpg"
+  },
+  {
+    "revision": "b2c46cc0171ebf49cbc3ba1766c09e9d",
+    "url": "/static/media/outline.b2c46cc0.PNG"
+  }
+]);
